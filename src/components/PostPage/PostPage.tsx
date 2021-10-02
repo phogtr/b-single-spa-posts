ffe7fc8127441b0abd703080985f3a57e92a03b6
phogtr <phogbtr@gmail.com>
2021-10-01 18:12:46 -0700

    add & set up router

--- src/components/PostPage/PostPage.tsx
+++ src/components/PostPage/PostPage.tsx
@@ -4,9 +4,9 @@ import "./PostPage.style.css";
 
 export const PostPage: React.FC<{}> = () => {
   return (
-    <>
+    <section>
       <h1>Posts</h1>
       <PostList />
-    </>
+    </section>
   );
 };
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,9 +1,14 @@
 import { PostPage } from "./components/PostPage/PostPage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default function Root() {
   return (
-    <section>
-      <PostPage />
-    </section>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <PostPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
